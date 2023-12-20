0b50f88b57538bfb219e2c082f3f49d6c235f950
Rou795 <jon.79555@gmail.com>
2023-12-20 17:40:24 +0700

    Выполнил дополнительное задание. Замечания устранил.

--- CSV_JSON/src/main/java/org/example/Main.java
+++ CSV_JSON/src/main/java/org/example/Main.java
@@ -7,10 +7,12 @@ import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.exceptions.CsvValidationException;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,16 +20,22 @@ public class Main {
     public static void main(String[] args) {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
+        File test = new File(fileName);
+        System.out.println(test.getAbsolutePath());
         parseCSV(fileName);
         List<Employee> list = parseCSV(columnMapping, fileName);
         list.forEach(System.out::println);
         String data = listToJson(list);
+        System.out.println(data);
         boolean isWriteToJson = writeString(data);
         if (isWriteToJson) {
             System.out.println("JSON-file saved.");
         } else {
             System.out.println("JSON-file not saved.");
         }
+        System.out.println("*******************************\n");
+        String jsonRow = readString("data.json");
+        jsonToList(jsonRow).forEach(System.out::println);
     }
 
 // метод для чтения CSV в объекты
@@ -56,6 +64,7 @@ public class Main {
         try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
             String[] nextLine;
             while ((nextLine = reader.readNext()) != null) {
+                System.out.println(nextLine.length);
                 System.out.println(Arrays.toString(nextLine));
             }
         } catch (IOException | CsvValidationException e) {
@@ -70,15 +79,8 @@ public class Main {
         Gson gson = builder
                 .setPrettyPrinting()
                 .create();
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("[\n");
-        for (Employee employee : employees) {
-            stringBuilder.append(gson.toJson(employee));
-            stringBuilder.append(",\n");
-        }
-        stringBuilder.replace(stringBuilder.length() - 2, stringBuilder.length() - 1, "");
-        stringBuilder.append("]");
-        return stringBuilder.toString();
+        String row = gson.toJson(employees);
+        return row;
     }
 
 // метод для записи строки в JSON-файл
@@ -92,5 +94,39 @@ public class Main {
             System.out.println(ex.getMessage());
         }
         return false;
+    }
+
+    // метод для чтения json и выдачи данных в виде строки
+
+    private static String readString(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String row;
+            while ((row = bufferedReader.readLine()) != null) {
+                stringBuilder.append(row);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return stringBuilder.toString();
+    }
+
+// метод для получения списка объектов из json-строки
+
+    private static List<Employee> jsonToList(String data) {
+        List<Employee> list = new ArrayList<>();
+        JSONParser parser = new JSONParser();
+        try {
+            Object object = parser.parse(data);
+            JSONArray jsonArray = (JSONArray) object;
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            for (Object element : jsonArray) {
+                list.add(gson.fromJson(element.toString(), Employee.class));
+            }
+        } catch (ParseException ex) {
+            ex.getMessage();
+        }
+        return list;
     }
 }
--- XML_JSON/src/main/java/org/example/Main.java
+++ XML_JSON/src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -11,9 +16,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +32,43 @@ public class Main {
         } else {
             System.out.println("JSON-file not saved.");
         }
+        String json = readString("data.json");
+        System.out.println("*******************************\n");
+        jsonToList(json).forEach(System.out::println);
+    }
+
+// метод для чтения json и выдачи данных в виде строки
+
+    private static String readString(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String row;
+            while ((row = bufferedReader.readLine()) != null) {
+                stringBuilder.append(row);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return stringBuilder.toString();
+    }
+
+// метод для получения списка объектов из json-строки
+
+    private static List<Employee> jsonToList(String data) {
+        List<Employee> list = new ArrayList<>();
+        JSONParser parser = new JSONParser();
+        try {
+            Object object = parser.parse(data);
+            JSONArray jsonArray = (JSONArray) object;
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            for (Object element : jsonArray) {
+                list.add(gson.fromJson(element.toString(), Employee.class));
+            }
+        } catch (ParseException ex) {
+            ex.getMessage();
+        }
+        return list;
     }
 
 // метод для чтения XML и занесения информаици в объект. Запись делаю в цикле через конструктор.
@@ -83,15 +123,8 @@ public class Main {
         Gson gson = builder
                 .setPrettyPrinting()
                 .create();
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("[\n");
-        for (Employee employee : employees) {
-            stringBuilder.append(gson.toJson(employee));
-            stringBuilder.append(",\n");
-        }
-        stringBuilder.replace(stringBuilder.length() - 2, stringBuilder.length() - 1, "");
-        stringBuilder.append("]");
-        return stringBuilder.toString();
+        String row = gson.toJson(employees);
+        return row;
     }
 
     public static boolean writeString(String data) {
